83a3749773aba2a1ad251608f91257b86c391010
Thales Cesar Pires <thalescesarp@gmail.com>
2018-06-23 18:53:40 -0300

    Unnused extension methods removed

--- todomvc-ui/src/utils/Utils.kt
+++ todomvc-ui/src/utils/Utils.kt
@@ -6,25 +6,6 @@ import kotlin.js.Date
 val Event.value: String
     get() = this.currentTarget.asDynamic().value as String
 
-val Event.rawValue: Any?
-    get() = {
-        val value = this.currentTarget.asDynamic().value
-
-        when (jsTypeOf(value)) {
-            "string" -> value as String
-            "number" -> value as Float
-            "boolean" -> value as Boolean
-            "null" -> null
-            "undefined" -> null
-
-            else -> value
-        }
-    }
-
-fun Date.toISO(): String {
-    return this.toISOString().split("T")[0]
-}
-
 fun Date.toLocalString(): String {
     return this.toLocaleDateString(kotlin.browser.window.navigator.language)
 }
